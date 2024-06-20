fb54778e9672fc69f77e7c1b188ce5a97243585f
Kenneth Aladi <kendevops@gmail.com>
2024-06-20 12:53:25 +0100

    created the profile icon with drop-down features

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,61 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
-      <section className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between '>
-          <Link href="/">
-              <Image src="/img/logo.png" width={40} alt="Synergetix logo" />
-          </Link>
-    </section>
-  )
-}
+    <section className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between ">
+      <Link href="/">
+        <Image
+          src="/img/logo.png"
+          width={40}
+          height={40}
+          alt="Synergetix logo"
+        />
+      </Link>
 
-export default Navbar
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shascn" />
+            <AvatarFallback className="text-black">BT</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span>Profile</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Billing</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Settings</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Keyboard shortcuts</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <span>Log out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </section>
+  );
+};
+
+export default Navbar;
